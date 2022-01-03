da5ed388149bcc6e929d1f2c38aee836a9e2d1c9
Archi711 <chwedczukarek@gmail.com>
2022-01-03 17:15:15 +0100

    fix(dev): update validations

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,16 +1,27 @@
 import * as Yup from 'yup'
 
 export const LoginBodyValidation = Yup.object({
-  login: Yup.string().required('Login jest wymagany'),
-  password: Yup.string().required('Hasło jest wymagane'),
+  login: Yup.string().strict().required(),
+  password: Yup.string().strict().required(),
 })
 
 export const RegisterBodyValidation = Yup.object({
-  login: Yup.string().required('Login jest wymagany'),
+  login: Yup.string()
+    .matches(
+      /^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/,
+      'Username must contain only alphanumeric characters, underscore or dot - min 8 - max 20 characters'
+    )
+    .min(8)
+    .max(20)
+    .required()
+    .trim()
+    .strict(),
   password: Yup.string()
-    .required('Hasło jest wymagane')
-    .min(8, 'Hasło musi mieć co najmniej 8 znaków'),
-  email: Yup.string()
-    .email('Podaj poprawny email')
-    .required('Poprawny email jest wymagany'),
+    .strict()
+    .required()
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+      'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character'
+    ),
+  email: Yup.string().email().required(),
 })
