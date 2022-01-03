53ea155ad0b77044cf225276253e8dc76017644e
Archi711 <chwedczukarek@gmail.com>
2022-01-03 17:51:06 +0100

    feat(dev): update profile page, base layout for profile settings

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -1,4 +1,58 @@
-export default function Profile() {
+import {
+  Badge,
+  Button,
+  Divider,
+  Grid,
+  GridItem,
+  HStack,
+  Text,
+} from '@chakra-ui/react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useAppSelector } from '../../app/hooks'
+import AppLink from '../custom/AppLink'
 
-  return <div>Profile</div>
+export default function Profile() {
+  const params = useParams()
+  const navigate = useNavigate()
+  const userData = useAppSelector((state) => state.auth.user)
+  const isOwn = params.id === 'self'
+  const profileId = isOwn ? userData?.id : Number(params.id)
+  if (!userData?.id && params.id === 'self') {
+    navigate('/login', {
+      state: {
+        from: '/profile/self',
+        message: profileId ? 'Unauthorized' : 'Unknown error',
+      },
+    })
+  }
+
+  return (
+    <Grid templateColumns='repeat(3, 1fr)' gap={2}>
+      <GridItem colSpan={1} border='1px solid #ccc' borderRadius={6} p={2}>
+        <Text fontSize={24}>Profile data: </Text>
+        <Divider />
+        <HStack>
+          <Badge>Name:</Badge>
+          <Text>{userData?.name}</Text>
+        </HStack>
+        <Divider />
+        <HStack>
+          <Badge>GithubNick:</Badge>
+          <Text>{userData?.githubNick}</Text>
+        </HStack>
+        <Divider />
+        <HStack>
+          <Badge>Description:</Badge>
+          <Text>{userData?.profileDescription}</Text>
+        </HStack>
+        <Divider />
+        {isOwn ? (
+          <AppLink to={`/profile/self/settings`}>
+            <Button>Edit</Button>
+          </AppLink>
+        ) : null}
+      </GridItem>
+      <GridItem colSpan={3}></GridItem>
+    </Grid>
+  )
 }
--- src/components/pages/ProfileSettings.tsx
+++ src/components/pages/ProfileSettings.tsx
@@ -1,4 +1,22 @@
+import { Center, Stack } from '@chakra-ui/react'
+import { Form } from 'formik'
+import { useNavigate } from 'react-router-dom'
+import { useAppSelector } from '../../app/hooks'
 export default function ProfileSettings() {
-
-  return <div>ProfileSettings</div>
+  const navigate = useNavigate()
+  const userData = useAppSelector((state) => state.auth.user)
+  if (!userData)
+    navigate('/login', {
+      state: {
+        from: '/profile/self/settings',
+        message: 'You must log in to access this page',
+      },
+    })
+  return (
+    <Stack>
+      <Center>
+        <Stack as={Form}></Stack>
+      </Center>
+    </Stack>
+  )
 }
