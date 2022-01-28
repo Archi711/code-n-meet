f2e311dcbb0735ce2184cbab4e57b078a60df6ef
Archi711 <chwedczukarek@gmail.com>
2022-01-28 16:23:31 +0100

    fix: fix navigation in (group)Users

--- src/components/features/group/Users.tsx
+++ src/components/features/group/Users.tsx
@@ -1,21 +1,28 @@
-import { Divider, Heading, LinkBox, LinkOverlay, Stack, Text } from '@chakra-ui/react';
-import AppLink from '../../custom/AppLink';
+import {
+  Divider,
+  Heading,
+  LinkBox,
+  LinkOverlay,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
+import AppLink from '../../custom/AppLink'
 
 type GroupUsersProps = {
   users: {
-    id: number,
+    id: number
     name: string
   }[]
   owner: {
-    id: number,
+    id: number
     name: string
   }
 }
 
-const UserLink = (props: { user: { id: number, name: string } }) => (
+const UserLink = (props: { user: { id: number; name: string } }) => (
   <LinkBox>
     <Text>
-      <LinkOverlay as={AppLink} to={`users/${props.user.id}`}>
+      <LinkOverlay as={AppLink} to={`/profile/${props.user.id}`}>
         {props.user.name}
       </LinkOverlay>
     </Text>
@@ -23,14 +30,21 @@ const UserLink = (props: { user: { id: number, name: string } }) => (
 )
 
 export default function GroupUsers(props: GroupUsersProps) {
-  return <Stack><Heading size='lg' as='h3'>Users</Heading>
+  return (
     <Stack>
-      <Text fontSize='sm'>Owner</Text>
-      <UserLink user={props.owner} />
-      <Divider />
-      {
-        props.users.map((user, idx) => user.id !== props.owner.id ? <UserLink key={`userLink-${idx}-${user.id}`} user={user} /> : null)
-      }
+      <Heading size='lg' as='h3'>
+        Users
+      </Heading>
+      <Stack>
+        <Text fontSize='sm'>Owner</Text>
+        <UserLink user={props.owner} />
+        <Divider />
+        {props.users.map((user, idx) =>
+          user.id !== props.owner.id ? (
+            <UserLink key={`userLink-${idx}-${user.id}`} user={user} />
+          ) : null
+        )}
+      </Stack>
     </Stack>
-  </Stack>
+  )
 }
