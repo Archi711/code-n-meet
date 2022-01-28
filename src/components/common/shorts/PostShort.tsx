ce6d66751d899d899b93238de05583c7d8e174df
Archi711 <chwedczukarek@gmail.com>
2022-01-28 16:23:50 +0100

    feat: update PostShort UI

--- src/components/common/shorts/PostShort.tsx
+++ src/components/common/shorts/PostShort.tsx
@@ -1,15 +1,35 @@
-import { Text, VStack } from '@chakra-ui/react'
+import { Heading, LinkBox, LinkOverlay } from '@chakra-ui/react'
 import { PostResponse } from '../../../types'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
+import { Link } from 'react-router-dom'
 
 export default function PostShort({ post }: { post: PostResponse }) {
   return (
-    <VStack>
-      <Text>{post.title}</Text>
+    <LinkBox
+      as='article'
+      p='5'
+      borderWidth='1px'
+      rounded='md'
+      sx={{
+        transition: '.1s all',
+        '&:hover': {
+          borderColor: 'rgba(255,255,255, .3)',
+        },
+      }}
+    >
+      <Heading size='md'>
+        <LinkOverlay
+          as={Link}
+          to={`/groups/${post.Group.id}/posts/${post.id}`}
+          href={`/groups/${post.Group.id}/posts/${post.id}`}
+        >
+          {post.title}
+        </LinkOverlay>
+      </Heading>
       <ReactMarkdown components={ChakraUIRenderer()} skipHtml>
-        {post.content.substring(50).concat('...')}
+        {post.content.substring(0, 50).concat('...')}
       </ReactMarkdown>
-    </VStack>
+    </LinkBox>
   )
 }
