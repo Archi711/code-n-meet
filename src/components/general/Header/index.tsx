0923d8bcee3250105b8ae97f6727c6dd8e2d297d
Archi711 <mlody711@o2.pl>
2022-02-01 20:46:35 +0100

    feat: update header

--- src/components/general/Header/index.tsx
+++ src/components/general/Header/index.tsx
@@ -1,12 +1,13 @@
 import { Button, Flex, Spacer } from '@chakra-ui/react'
 import AppLink from '../../custom/AppLink'
 import HeaderNav from './HeaderNav'
+import Logo from '../../../assets/logo.svg'
 
 export default function AppHeader() {
   return (
     <Flex as='header' p='1em'>
       <AppLink to='/'>
-        <Button>LOGO</Button>
+        <Button><img src={Logo} style={{ width: '100%', height: '100%' }} /> </Button>
       </AppLink>
       <Spacer />
       <HeaderNav />
